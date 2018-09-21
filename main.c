1cc1e2fed9dd905b0cb449c33450be4bfd72318c
liucanbin <liucanbin@cmiot.chinamobile.com>
2018-09-21 09:48:18 +0800

    add main.c

---
+++ main.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+int main()
+{
+	printf("hello,world!");
+	return 0;
+}
+/*
+ *
+ *a simple C program 
+ *
+ * */
+
+/*
+Git is a distributed version control system.
+Git is free software distributed under the GPL.
+Git has a mutable index called stage.
+*/
